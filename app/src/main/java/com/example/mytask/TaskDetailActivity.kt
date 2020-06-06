d3897f44ad7458a3987a68245564b7e2c4672962
afnankhattak <hmafnan3@gmail.com>
2020-06-06 16:32:39 +0400

    Created UI

---
+++ app/src/main/java/com/example/mytask/TaskDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mytask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_detail)
+    }
+}
